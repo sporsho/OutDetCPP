46c1c42669f91b8f65422fd21a9e83c7c6802e5f
Abu Mohammed Raisuddin <abu-mohammed.raisuddin@hh.se>
2024-10-31 13:29:49 +0100

    weight conversion from Python to C++

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 * Convert 3D-OutDet PyTorch model to C++ (done)
 * Data processing in CPP (in a week)
 * Train / Eval in CPP (in few hours)
-* Weight Conversion in CPP (in few hours)
+* Weight Conversion in CPP (done)
 * TensorRT + CPP (in 2 days)
 * ROS Integration (in 2 days)
 * Docker Integration (in 1 days)
--- main.cpp
+++ main.cpp
@@ -12,11 +12,63 @@
 #include<helper_cuda.h>
 #include<dlfcn.h>
 #include<stdlib.h>
+#include <assert.h>
 #include "src/knncuda.h"
+#include <torch/script.h>
 //#include <rmm/cuda_stream_view.hpp>
 //#include <cuml/common/logger.hpp>
 //#include<cuml/neighbors/knn.hpp>
 using namespace torch::indexing;
+bool load_cpp_weights(OutDet model){
+    // load model weights
+    torch::jit::script::Module cpp_module = torch::jit::load("/var/local/home/aburai/outdet_cpp/cpp_weights.pt");
+    // nh conv layer 1
+    assert(cpp_module.hasattr("convs.0.conv1.dw") == true && "Cannot read convs.0.conv1.dw");
+    model->conv1->conv1->dw = cpp_module.attr("convs.0.conv1.dw").toTensor();
+    assert(cpp_module.hasattr("convs.0.conv1.b") == true && "Cannot read convs.0.conv1.b");
+    model->conv1->conv1->b = cpp_module.attr("convs.0.conv1.b").toTensor();
+    assert(cpp_module.hasattr("convs.0.conv1.weight") == true && "Cannot read convs.0.conv1.weight");
+    model->conv1->conv1->weight = cpp_module.attr("convs.0.conv1.weight").toTensor();
+    // batch norm 1
+    assert(cpp_module.hasattr("convs.0.bn1.weight") == true && "Cannot read convs.0.bn1.weight");
+    model->conv1->bn1->weight = cpp_module.attr("convs.0.bn1.weight").toTensor();
+    assert(cpp_module.hasattr("convs.0.bn1.bias") == true && "Cannot read convs.0.bn1.bias");
+    model->conv1->bn1->bias = cpp_module.attr("convs.0.bn1.bias").toTensor();
+    assert(cpp_module.hasattr("convs.0.bn1.running_mean") == true && "Cannot read convs.0.bn1.running_mean");
+    model->conv1->bn1->running_mean = cpp_module.attr("convs.0.bn1.running_mean").toTensor();
+    assert(cpp_module.hasattr("convs.0.bn1.running_var") == true && "Cannot read convs.0.bn1.running_var");
+    model->conv1->bn1->running_var = cpp_module.attr("convs.0.bn1.running_var").toTensor();
+    assert(cpp_module.hasattr("convs.0.bn1.num_batches_tracked") == true && "Cannot read convs.0.bn1.num_batches_tracked");
+    model->conv1->bn1->num_batches_tracked = cpp_module.attr("convs.0.bn1.num_batches_tracked").toTensor();
+
+    // nh conv layer 2
+    assert(cpp_module.hasattr("convs.0.conv2.weight") == true && "Cannot read convs.0.conv2.weight");
+    model->conv1->conv2->weight = cpp_module.attr("convs.0.conv2.weight").toTensor();
+
+    //batch norm 2
+    assert(cpp_module.hasattr("convs.0.bn2.weight") == true && "Cannot read convs.0.bn2.weight");
+    model->conv1->bn2->weight = cpp_module.attr("convs.0.bn2.weight").toTensor();
+    assert(cpp_module.hasattr("convs.0.bn2.bias") == true && "Cannot read convs.0.bn2.bias");
+    model->conv1->bn2->bias = cpp_module.attr("convs.0.bn2.bias").toTensor();
+    assert(cpp_module.hasattr("convs.0.bn2.running_mean") == true && "Cannot read convs.0.bn2.running_mean");
+    model->conv1->bn2->running_mean = cpp_module.attr("convs.0.bn2.running_mean").toTensor();
+    assert(cpp_module.hasattr("convs.0.bn2.running_var") == true && "Cannot read convs.0.bn2.running_var");
+    model->conv1->bn2->running_var = cpp_module.attr("convs.0.bn2.running_var").toTensor();
+    assert(cpp_module.hasattr("convs.0.bn2.num_batches_tracked") == true && "Cannot read convs.0.bn2.num_batches_tracked");
+    model->conv1->bn2->num_batches_tracked = cpp_module.attr("convs.0.bn2.num_batches_tracked").toTensor();
+
+    // residual connection
+    assert(cpp_module.hasattr("convs.0.downsample.weight") == true && "Cannot read convs.0.downsample.weight");
+    model->conv1->downsample->weight = cpp_module.attr("convs.0.downsample.weight").toTensor();
+
+    // linear layer
+    assert(cpp_module.hasattr("fc.weight") == true && "Cannot read fc.weight");
+    model->fc->weight = cpp_module.attr("fc.weight").toTensor();
+    assert(cpp_module.hasattr("fc.bias") == true && "Cannot read fc.bias");
+    model->fc->bias = cpp_module.attr("fc.bias").toTensor();
+
+return true;
+}
 
 int main(int argc, char **argv)
 {
@@ -84,8 +136,10 @@ int main(int argc, char **argv)
         model->to(device);
         auto out = model->forward(inp.to(device), dist.to(device), ind.to(device));
 //        std::cout << out << std::endl;
-
-
+//        torch::save(model->parameters(), "mymodel.pt");
+        std::cout << model->conv1->conv1->dw << std::endl;
+        load_cpp_weights(model);
+        std::cout << model->conv1->conv1->dw << std::endl;
     }
     catch (const std::exception &ex){
         std::cerr << ex.what() << std::endl;
